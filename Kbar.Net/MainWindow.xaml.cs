85cfc798d55ccbb79999abc9aab121cbf693b187
KIMCHIway <suushiway@gmail.com>
2019-04-20 02:01:36 +0900

    First Init

---
+++ Kbar.Net/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kbar.Net
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LowKeyListener _listener;
+
+        private bool isTurn;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void _listener_OnKeyPressed(object sender, KeyPressedArgs e)
+        {
+            if (e.KeyPressed.ToString() == "F8")
+            {
+                if (isTurn)
+                {
+                    Visibility = Visibility.Hidden;
+                    isTurn = false;
+                }
+                else
+                {
+                    Visibility = Visibility.Visible;
+                    isTurn = true;
+                }
+            }
+
+            if (e.KeyPressed.ToString() == "Escape")
+            {
+                Visibility = Visibility.Hidden;
+                isTurn = false;
+            }
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowInTaskbar = false;
+            Topmost = true;
+
+            Top = (SystemParameters.PrimaryScreenHeight / 6) - (Height / 2);
+            Visibility = Visibility.Hidden;
+
+            // Code for hook key
+            _listener = new LowKeyListener();
+            _listener.OnKeyPressed += _listener_OnKeyPressed;
+
+            _listener.HookKeyboard();
+        }
+
+        private void CommandBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Wait for 1.5 s to check whether user will not type anymore.
+
+            // Split by blank (0 is Command)
+        }
+
+        //private void Grid_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        //{
+        //    _listener.UnHookKeyboard();
+        //}
+    }
+}
