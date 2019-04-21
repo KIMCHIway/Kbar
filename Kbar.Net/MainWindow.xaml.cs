645661a3c21de4057ac8d7aee2006aa1a7799e8a
KIMCHIway <suushiway@gmail.com>
2019-04-22 01:23:25 +0900

    UI and code of Papago module

--- Kbar.Net/MainWindow.xaml.cs
+++ Kbar.Net/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Console;
 
 // Kbar command docs : https://docs.google.com/document/d/1-0STF1GnlQi-IYj8PNtPBaO657w1jOwlGT1H6X81nZw/edit?usp=sharing
 // Code of GUI MainWindows.xaml 
@@ -23,17 +25,14 @@ namespace Kbar.Net
     public partial class MainWindow : Window
     {
         private LowKeyListener _listener;
-		private readonly DispatcherTimer timer;
 
+        private dynamic cur_SubWindow;
+        private dynamic cur_Module;
         private bool isTurn;
 
         public MainWindow()
         {
             InitializeComponent();
-			
-			// Code for input timer
-			timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1.5f) };
-			timer.Tick += HandleCommand;
 			
 			// Code for hook key
             _listener = new LowKeyListener();
@@ -54,12 +53,14 @@ namespace Kbar.Net
 
         void _listener_OnKeyPressed(object sender, KeyPressedArgs e)
         {
+            // Hotkey for window
             if (e.KeyPressed.ToString() == "F8")
             {
                 if (isTurn)
                 {
                     Visibility = Visibility.Hidden;
                     isTurn = false;
+
                 }
                 else
                 {
@@ -73,48 +74,97 @@ namespace Kbar.Net
                 Visibility = Visibility.Hidden;
                 isTurn = false;
             }
+
+
+            // Hotkey for command
+            if (e.KeyPressed.ToString() == "Return")
+            {
+                CommandMethod();
+            }
         }
 
 
         private void CommandBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            // Wait for 1.5s to check whether user will not type anymore.
-			timer.Stop();
-			timer.Start();
+            // module check
         }
 		
-		private void HandleCommand(object sender, EventArgs e)
+		private void CommandMethod()
 		{
-		    timer.Stop();
-			
-		    // Split by blank (0 is Command)
-			string[] command = CommandBox.Text.Split(' ');
-			if (command.Length > 0)
-			{
-			    string module = command[0];
-				switch (module)
-				{
-				    case "papago":
-				    case "ntrans":
-					case "ntranslation":
-					case "trans":
-					case "translation":
-					    if (command.Length >= 4)
-						{
-						    Papago papago = new Papago();
-							
-							// Close sub form which is showed
-							
-							// Turn on new sub form
-							
-							// Write information
-						}
-						
-						break;
-				}
-			}
-			
-		}
+            // Thread ?
+            // Split by blank (0 is Command)
+            string[] command = CommandBox.Text.Split(' ');
+
+            if (command.Length > 0)
+            {
+                string module = command[0];
+                switch (module)
+                {
+                    // papago module command
+                    case "papago":
+                    case "nt":
+                    case "ntranslation":
+                    case "translation":
+                        if (command.Length >= 4)
+                        {
+                            // Close sub form which is showed
+                            if (cur_SubWindow != null) cur_SubWindow.Close();
+
+                            // Turn on new sub form
+                            PapagoWindow subWindow = new PapagoWindow();
+                            Load_SubWindow(subWindow);
+
+                            // Combine seperated source text (INDEX 0:papago 1:en 2:ko 3:text1 4:text2 ~
+                            string sourceText = string.Empty;
+                            for (int i = 3; i < command.Length; i++)
+                            {
+                                sourceText += command[i] + " ";
+                            }
+
+                            // Call Papago API
+                            Papago papago = new Papago();
+                            string targetText = papago.Call_Papago(command[1], command[2], sourceText);
+
+                            // Allocate state variable
+                            Allocate_State(subWindow, papago);
+
+
+                            // Set window component
+                            subWindow.Label_sCode.Content = command[1];
+                            subWindow.Label_tCode.Content = command[2];
+                            subWindow.Label_sText.Content = sourceText;
+                            subWindow.Label_tText.Content = targetText;
+                        }
+
+                        break;
+                }
+            }
+            else if (command.Length == 0)
+            {
+                // if Sub Windows is showed, intialize state variable
+                if (cur_SubWindow != null)
+                {
+                    cur_SubWindow.Close();
+                    cur_SubWindow = null;
+
+                    cur_Module = null;
+                }
+            }
+        }
+
+
+        private void Load_SubWindow(dynamic window)
+        {
+            window.Show();
+            window.Left = Left;
+            window.Top = Top + Height - 10;
+        }
+
+        private void Allocate_State(dynamic subWindow, dynamic module)
+        {
+            cur_SubWindow = subWindow;
+            cur_Module = module;
+        }
 
         //private void Grid_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         //{
--- Kbar.Net/Papago.cs
+++ Kbar.Net/Papago.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +12,29 @@ namespace Kbar.Net
 {
     class Papago
     {
-		public void CallPapago()
+        string sourceCode;
+        string targetCode;
+        string sourceText;
+
+		public string Call_Papago(string sCode, string tCode, string sText)
 		{
-			string url = "https://openapi.naver.com/v1/papago/n2mt";
+            // Save parameter
+            sourceCode = sCode;
+            targetCode = tCode;
+            sourceText = sText;
+
+
+            // Set API setting
+            string url = "https://openapi.naver.com/v1/papago/n2mt";
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Headers.Add("X-Naver-Client-Id", "bX5qcykEM6KdYV9cscfR");
-            request.Headers.Add("X-Naver-Client-Secret", "VGzJd570sN  ");
+            request.Headers.Add("X-Naver-Client-Secret", "VGzJd570sN");
             request.Method = "POST";
-            string query = "오늘 날씨는 어떻습니까?";
-            byte[] byteDataParams = Encoding.UTF8.GetBytes("source=ko&target=en&text=" + query);
+
+            // Handle data ( Source language , Target language , Source text )
+            string data = string.Format("source={0}&target={1}&text={2}", sCode, tCode, sText);
+            byte[] byteDataParams = Encoding.UTF8.GetBytes(data);
+
             request.ContentType = "application/x-www-form-urlencoded";
             request.ContentLength = byteDataParams.Length;
             Stream st = request.GetRequestStream();
@@ -27,11 +43,21 @@ namespace Kbar.Net
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Stream stream = response.GetResponseStream();
             StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-            string text = reader.ReadToEnd();
+
+            // Target text
+            string[] result = reader.ReadToEnd().Split(':');
+            string tText = result[8].Split('"')[1];
+
             stream.Close();
             response.Close();
             reader.Close();
-            Console.WriteLine(text);
+
+            return tText;
 		}
+
+        public void Connect_Site()
+        {
+
+        }
     }
 }
